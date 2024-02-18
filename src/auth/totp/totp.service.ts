8aaed77423e25d7437c7baf9b38e87c0a6a028ab
Andreas Lord <andlo779@gmail.com>
2024-02-18 11:05:26 +0100

    #4: refactoring- extracted hard coded string to conf

--- src/auth/totp/totp.service.ts
+++ src/auth/totp/totp.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { authenticator } from 'otplib';
 import { authenticator as authDataGen } from '@otplib/preset-default';
+import { ConfigService } from '@nestjs/config';
+import { AUTH_ISSUER } from '../../constants';
 
 Injectable();
 export class TotpService {
+  constructor(private readonly configService: ConfigService) {}
   generateSecrete(): string {
     return authenticator.generateSecret();
   }
 
   generateQrUri(userName: string, secret: string): string {
-    return authDataGen.keyuri(userName, 'concrete', secret);
+    const issuer = this.configService.get<string>(AUTH_ISSUER);
+    return authDataGen.keyuri(userName, issuer, secret);
   }
 
   validateToken(token: string, secret: string): boolean {
